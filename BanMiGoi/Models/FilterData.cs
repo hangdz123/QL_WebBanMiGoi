03b227a00aee1cd442b49890aa1c0d602aa5aa8c
hangdz123 <112870660+hangdz123@users.noreply.github.com>
2025-01-11 17:17:50 +0700

    Update FilterData.cs

--- BanMiGoi/Models/FilterData.cs
+++ BanMiGoi/Models/FilterData.cs
@@ -1,4 +1,4 @@
-﻿namespace ThanhThoaiRestaurant.Models
+namespace ThanhThoaiRestaurant.Models
 {
     public class FilterData
     {
@@ -8,6 +8,7 @@
         public List<string> Cpu { get; set; }
         public List<string> Screen { get; set; }
         public List<string> PriceRange { get; set; }
+        public string SortOption {  get; set; }
         public int? PageNumber { get; set; }
         public PagingInfo PagingInfo { get; set; } = new PagingInfo();
 
